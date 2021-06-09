a30a28fd964c15225734eb8641eda0d4a808eb65
Al-Rou <roal.24.alro@gmail.com>
2021-06-08 23:24:52 -0400

    Complete and commented

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#Prompt the user for entries
 enteredNumbers = input("Enter your numbers: ")
 a = ""
+#Defining the array and filling it out with all numbers
 arrayDef = []
 for i in range (len(enteredNumbers)):
     if((enteredNumbers[i] != ';') and (enteredNumbers[i] != ',')):
@@ -8,14 +10,16 @@ for i in range (len(enteredNumbers)):
         arrayDef.append(int(a))
         a = ""
 arrayDef.append(int(a))
+#Deleting the first entered number, as it is the length of array, not an entry
 arrayDef.pop(0)
+#Sorting the array
 for i in range (len(arrayDef)-1):
     for j in range(i+1, len(arrayDef)):
         if(arrayDef[i] > arrayDef[j]):
             temp = arrayDef[i]
             arrayDef[i] = arrayDef[j]
             arrayDef[j] = temp
-
+#Finding two neighbors which are the same
 for i in range (len(arrayDef)-1):
     if (arrayDef[i] == arrayDef[i+1]):
         print(arrayDef[i])
